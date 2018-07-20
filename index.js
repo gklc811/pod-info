6aebe14d59ffc4cade7e6ae56b0c71b4f4fd8561
gokul <gokul.chinnasamy@aline-consulting.com>
2018-07-20 17:32:23 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -4,55 +4,73 @@ var os = require('os')
 var nocache = require('nocache')
 var roundTo = require('round-to');
 
+require('dotenv').config()
+
 var app = express()
 app.use(cors())
 app.use(nocache())
 
+var Client = require('node-rest-client').Client;
+
+var client = new Client();
+
+
 app.set('port', (process.env.PORT || 5000))
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
     response.send('Hello World!')
 })
 
-app.get('/podinfo', function(request, response) {
+app.get('/podinfo', function (request, response) {
     var data = {
         'Pod Host': os.hostname(),
         'Pod uptime': os.uptime() + ' secs',
         'Pod CPU load': os.loadavg(),
-        'Pod Total Memory': roundTo(os.totalmem() / (1024 * 1024 * 1024),2) + ' GB',
-        'Pod Free Memory': roundTo(os.freemem() / (1024 * 1024 * 1024),2) + ' GB',
-		'Pod CPU Count': os.cpus().length
+        'Pod Total Memory': roundTo(os.totalmem() / (1024 * 1024 * 1024), 2) + ' GB',
+        'Pod Free Memory': roundTo(os.freemem() / (1024 * 1024 * 1024), 2) + ' GB',
+        'Pod CPU Count': os.cpus().length
     }
     response.send(data)
 })
 
-app.get('/route1', function(request, response) {
-	    var data = {
-		'Hit route':'route1',
+app.get('/route1', function (request, response) {
+    var data = {
+        'Hit route': 'route1',
         'Pod Host': os.hostname(),
         'Pod uptime': os.uptime() + ' secs',
         'Pod CPU load': os.loadavg(),
-        'Pod Total Memory': roundTo(os.totalmem() / (1024 * 1024 * 1024),2) + ' GB',
-        'Pod Free Memory': roundTo(os.freemem() / (1024 * 1024 * 1024),2) + ' GB',
-		'Pod CPU Count': os.cpus().length
+        'Pod Total Memory': roundTo(os.totalmem() / (1024 * 1024 * 1024), 2) + ' GB',
+        'Pod Free Memory': roundTo(os.freemem() / (1024 * 1024 * 1024), 2) + ' GB',
+        'Pod CPU Count': os.cpus().length
     }
     response.send(data)
 })
 
-app.get('/route2', function(request, response) {
-	    var data = {
-		'Hit route':'route2',
+app.get('/crossapi', function (request, response) {
+    if (process.env.CROSS_API_URL) {
+        client.get(process.env.CROSS_API_URL, function (data, res) {
+            response.send(data)
+        });
+    }
+    else {
+        response.send("Cross API URL Not Defined")
+    }
+})
+
+app.get('/route2', function (request, response) {
+    var data = {
+        'Hit route': 'route2',
         'Pod Host': os.hostname(),
         'Pod Uptime': os.uptime() + ' secs',
         'Pod CPU load': os.loadavg(),
-        'Pod Total Memory': roundTo(os.totalmem() / (1024 * 1024 * 1024),2) + ' GB',
-        'Pod Free Memory': roundTo(os.freemem() / (1024 * 1024 * 1024),2) + ' GB',
-		'Pod CPU Count': os.cpus().length
+        'Pod Total Memory': roundTo(os.totalmem() / (1024 * 1024 * 1024), 2) + ' GB',
+        'Pod Free Memory': roundTo(os.freemem() / (1024 * 1024 * 1024), 2) + ' GB',
+        'Pod CPU Count': os.cpus().length
     }
     response.send(data)
 })
 
-app.get('/crasher', function(request, response) {
+app.get('/crasher', function (request, response) {
     // process.exit(1)
     // response.send("Crashed...")
     process.nextTick(function () {
@@ -60,6 +78,6 @@ app.get('/crasher', function(request, response) {
     });
 })
 
-app.listen(app.get('port'),'0.0.0.0', function() {
+app.listen(app.get('port'), '0.0.0.0', function () {
     console.log("Node app is running at 0.0.0.0:" + app.get('port'))
 })
